c4ab136c67204db23e2c4038aeda0fb0ad3cf9cd
Mike C <mikecronin92@gmail.com>
2018-03-20 06:10:52 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,10 @@
 #write your code here
 
 def countdown(num)
-  until num == 0 do 
-    puts "#{num} SECOND(S)!"
-    count -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(num)
-  until num == 0 do 
+  count = num
+  until count == 0 do 
     sleep(1)
-    puts "#{num} SECOND(S)!"
+    puts "#{count} SECOND(S)!"
     count -= 1
   end
   "HAPPY NEW YEAR!"
